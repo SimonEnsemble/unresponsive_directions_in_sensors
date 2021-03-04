bf541ffecf7f6a824929138c9a54d14b0878ab7d
Nickolas Gantzler <gantzlen@oregonstate.edu>
2021-03-03 16:15:36 -0800

    worked on function to get the isotherm data for a specified (material, gas) pair

--- gas_material_matrix.jl
+++ gas_material_matrix.jl
@@ -5,7 +5,7 @@ using Markdown
 using InteractiveUtils
 
 # ╔═╡ 78df6c80-76f7-11eb-3995-05dedb662631
-using JSON
+using JSON, HTTP
 
 # ╔═╡ bc1d042e-76f7-11eb-3d59-1160a17c3b52
 biblios = JSON.parsefile("biblios.json")
@@ -52,6 +52,9 @@ begin
 	initial_gas_candidates = gas_names[initial_gas_candidate_ids]
 end
 
+# ╔═╡ 73e94a10-7c75-11eb-356a-ab551e22d8ef
+
+
 # ╔═╡ 42f21322-7797-11eb-1846-3167865e5265
 # water isn't a gas. propylene and ethane aren't that common in the lab.
 disqualified_gas_ids = [i for i ∈ 1:length(gases) if gas_names[i] ∈ ["Water", "Ethane", "Propene"]]
@@ -72,8 +75,88 @@ begin
 end
 
 # ╔═╡ a81f6722-7788-11eb-3cc7-7f6d18a64d75
-# materials that have isotherms for all candidate gases
-material_candidates = [m for (i, m) ∈ enumerate(materials) if all(material_gas_matrix[i, gas_candidate_ids] .== 1)]
+# # materials that have isotherms for all candidate gases
+# material_candidates = [m for (i, m) ∈ enumerate(materials) if all(material_gas_matrix[i, gas_candidate_ids] .== 1)]
+
+# ╔═╡ 3bd2ec08-7c75-11eb-1df2-91d7445431c4
+begin
+	gases_we_want = [
+		"Carbon Dioxide",
+		"Methane",
+		# "Hydrogen",
+		"Nitrogen",
+		# "Ethene",
+		# "Ethane",
+		# "N-Butane",
+		# "Oxygen",
+		# "Acetylene",
+		# "N-propane",
+		# "Propene",
+		# "Carbon monoxide"
+	]
+
+	gas_ids_we_want = [i for i ∈ 1:length(gases) if gas_names[i] ∈ gases_we_want]
+	
+	material_candidates = [m for (i, m) ∈ enumerate(materials) if all(material_gas_matrix[i, gas_ids_we_want] .== 1)]
+	
+end
+
+# ╔═╡ 93a1af92-7c79-11eb-30ad-37814e426a28
+begin
+	function get_material_hashkey(material_name::String)
+		for material in materials
+			if material["name"] == material_name
+				return material["hashkey"]
+			end
+		end
+		@error "$material_name not found"
+	end
+
+	function get_gas_inchekey(gas_name::String)
+		for gas in gases
+			if gas["name"] == gas_name
+				return gas["InChIKey"]
+			end
+		end
+		@error "$gas_name not found"
+	end
+
+
+	function get_isotherm(material_name::String, gas_name::String)
+		# get the hashkey for material
+		material_hashkey = get_material_hashkey(material_name)
+		# get InChiKey for gas
+		gas_inchekey = get_gas_inchekey(gas_name)
+
+		temperature_tolerance = 3
+		isotherms_we_want = []
+
+		for isotherm ∈ isotherms
+			if length(isotherm["adsorbates"]) ≠ 1 # get only single-component isotherms
+				continue
+			end
+
+			if isotherm["category"] ≠ "exp" # only take experimental isotherms
+				continue
+			end
+			# check temperature range
+			if isotherm["temperature"] > 298 + temperature_tolerance || isotherm["temperature"] < 298 - temperature_tolerance
+				continue
+			end
+			# is it for the 
+			if (isotherm["adsorbent"]["hashkey"] == material_hashkey) && (isotherm["adsorbates"][1]["InChIKey"] == gas_inchekey)
+
+				push!(isotherms_we_want, isotherm)
+			end
+		end
+		return isotherms_we_want
+	end
+	get_isotherm("CuBTC", "Carbon Dioxide")
+end
+
+# ╔═╡ 0c25e8e0-7c7d-11eb-1772-bdda2a6b4be0
+HTTP.request("POST", "http://adsorption.nist.gov/isodb/api/isotherm/10.1002adfm.200500561.Isotherm4.json"
+	)
 
 # ╔═╡ Cell order:
 # ╠═78df6c80-76f7-11eb-3995-05dedb662631
@@ -83,7 +166,11 @@ material_candidates = [m for (i, m) ∈ enumerate(materials) if all(material_gas
 # ╠═09d5252e-76f9-11eb-02e3-ff05abff717d
 # ╠═8f15156e-76f9-11eb-03fc-979d446be28a
 # ╠═6c4a7ff0-76fc-11eb-3e87-c71b75c8ebd3
+# ╠═73e94a10-7c75-11eb-356a-ab551e22d8ef
 # ╠═42f21322-7797-11eb-1846-3167865e5265
 # ╠═a2f3f780-7787-11eb-0db1-19c417563a5d
 # ╠═d91dada2-7797-11eb-3030-5b4bb8aad992
 # ╠═a81f6722-7788-11eb-3cc7-7f6d18a64d75
+# ╠═3bd2ec08-7c75-11eb-1df2-91d7445431c4
+# ╠═93a1af92-7c79-11eb-30ad-37814e426a28
+# ╠═0c25e8e0-7c7d-11eb-1772-bdda2a6b4be0
