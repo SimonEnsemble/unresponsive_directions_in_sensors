34dc88bde5991a63f21e82bb0b47bc8e5e4528d4
Adrian Henle <eahenle@gmail.com>
2021-05-12 15:49:23 -0700

    3-contam in mayavi

--- mayavi_viz.py
+++ mayavi_viz.py
@@ -1,16 +1,25 @@
 from mayavi import mlab
 import numpy as np
 
-def V(x, y, z):
-    """ A 3D sinusoidal lattice with a parabolic confinement. """
-    return 2*(x**2 + y**2 + z**2)
+mof = "Co-MOF-74"
+contaminants = ['N2', 'CO2', 'C2H6']
 
+reloaded = np.load("{}_{}_{}_{}.npz".format(mof, contaminants[0], contaminants[1], contaminants[2]))
+X1 = reloaded['X1']
+X2 = reloaded['X2']
+X3 = reloaded['X3']
+M = reloaded['M']
+v1 = reloaded['v1']
+v2 = reloaded['v2']
+v3 = reloaded['v3']
+x_op = reloaded['x_op']
 
-X, Y, Z = np.mgrid[-2:2:100j, -2:2:100j, -2:2:100j]
 mlab.figure(bgcolor=(1.0, 1.0, 1.0), fgcolor=(0.0, 0.0, 0.0))
-mlab.quiver3d(0, 0, 0, 1, 1, 1, color=(0.0, 0.0, 0.0), line_width=10, mode="arrow")
+mlab.quiver3d(x_op[0], x_op[1], x_op[2], v1[0], v1[1], v1[2], color=(0.0, 0.0, 0.0), line_width=10, mode="arrow", scale_factor=0.2)
+mlab.quiver3d(x_op[0], x_op[1], x_op[2], v2[0], v2[1], v2[2], color=(0.0, 0.0, 0.0), line_width=10, mode="arrow", scale_factor=0.2)
+mlab.quiver3d(x_op[0], x_op[1], x_op[2], v3[0], v3[1], v3[2], color=(0.0, 0.0, 0.0), line_width=10, mode="arrow", scale_factor=0.2)
 mlab.text(1.2, 1.2, "$v_1$", z=1.2)
-mlab.contour3d(X, Y, Z, V, colormap="viridis", opacity=0.3, contours=8, vmin=0.0)
-mlab.axes(xlabel="x", ylabel="y", zlabel="z")
+mlab.contour3d(X1, X2, X3, M, colormap="viridis", opacity=0.3, contours=8, vmin=0.0)
+mlab.axes(xlabel=contaminants[0], ylabel=contaminants[1], zlabel=contaminants[2])
 mlab.colorbar(title="m [g/g]", orientation="vertical")
 mlab.show()
