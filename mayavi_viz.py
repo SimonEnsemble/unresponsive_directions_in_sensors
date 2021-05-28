e4acbb817933b5953870824278a1f4773478bae6
SimonEnsemble <cory.simon@oregonstate.edu>
2021-05-27 23:08:02 -0700

    k

--- mayavi_viz.py
+++ mayavi_viz.py
@@ -1,10 +1,12 @@
 from mayavi import mlab
 import numpy as np
 import sys
+from mlabtex import mlabtex
 
 mof = sys.argv[1]
 contaminants = ['N2', 'CO2', 'C2H6']
-labels = {'N2': 'nitrogen', 'CO2': 'carbon dioxide', 'C2H6': 'ethane'}
+contaminant_to_label = {'N2': r'$x_{N_2}$', 'CO2': r'$x_{CO_2}$', 'C2H6': r'$x_{C_2H_6}$'}
+#labels = {'N2': 'nitrogen', 'CO2': 'carbon dioxide', 'C2H6': 'ethane'}
 cool_colors = {'green': (0.0, 0.716, 0.554), 'red': (1.0, 0.403, 0.397), 'blue': (0.0, 0.747, 1.0), "yellow": (0.830929, 0.793470, 0.225663), "purple": (0.745987, 0.663119, 1.000000)}
 
 # load data from .ipynb
@@ -39,18 +41,27 @@ mlab.quiver3d(x_op[0], x_op[1], x_op[2],
 c = mlab.contour3d(X1, X2, X3, M, colormap="viridis", opacity=0.25, contours=8, vmin=0.0)
 mlab.colorbar(c, title="m [g/g]", orientation="vertical")
 mlab.outline(c)
-mlab.axes(xlabel=labels[contaminants[0]], ylabel=labels[contaminants[1]], zlabel=labels[contaminants[2]])
+mlab.axes(
+xlabel="",  ylabel="", zlabel="", z_axis_visibility=False, x_axis_visibility=False
+#xlabel=labels[contaminants[0]], ylabel=labels[contaminants[1]], zlabel=labels[contaminants[2]]
+)
 mlab.view(0.0, 0.0)
-#mlab.pitch(float(sys.argv[2]))
-#mlab.roll(float(sys.argv[3]))
-#mlab.yaw(float(sys.argv[4]))
+mlab.pitch(-10.0)
+# mlab.roll(15.0)
+mlab.yaw(-45.0)
 # own axes
 x = np.linspace(0.0, np.max(X1))
 x_z = np.zeros_like(x)
-mlab.plot3d(x, x_z, x_z, line_width=0.01,tube_radius=0.001)
-mlab.plot3d(x_z, x, x_z, line_width=0.01,tube_radius=0.001)
-mlab.plot3d(x_z, x_z, x, line_width=0.01,tube_radius=0.001)
-mlab.text3d(np.max(X1) * 1.05, 0.0, 0.0, contaminants[0], color=(0, 0, 0), scale=0.025)
-mlab.text3d(0.0, np.max(X1) * 1.05, 0.0, contaminants[1], color=(0, 0, 0), scale=0.025)
-mlab.text3d(0.0, 0.0, np.max(X1) * 1.05, contaminants[2], color=(0, 0, 0), scale=0.025)
+for i in range(3):
+    mlab.plot3d(*[np.linspace(0.0, np.max(X1), 3) if i == j else np.zeros(3) for j in range(3)], line_width=0.01,tube_radius=0.001)
+#mlab.text3d(1.05 * np.max(X1), 0.0, 0.0, contaminants[1], scale=0.025)
+orientations = [(0.0, -45.0, 0.0), (0.0, -45.0, 0.0), (0.0, -45.0, 0.0)]
+for i in range(3):
+    tex = mlabtex(
+        *[1.05 * np.max(X1) if j == i else 0.0 for j in range(3)],
+        contaminant_to_label[contaminants[i]],
+        color=(0., 0., 0.),
+        orientation=orientations[i],
+        scale=0.025,
+        dpi=1200)
 mlab.show()
